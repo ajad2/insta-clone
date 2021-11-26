153baaaf9b6e3bc66d11d18bf4cc25e48a4feb79
ajad2 <ajad9223@gmail.com>
2021-11-26 20:51:07 +0530

    first commit

---
+++ app/Http/Controllers/ProfilesController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\User;
+
+use Illuminate\Http\Request;
+
+class ProfilesController extends Controller
+{
+    public function index($user)
+    {
+        $user_data = User::findOrFail($user);
+        // dd($data);
+        return view('profiles.index',['user'=>$user_data]);
+    }
+}
